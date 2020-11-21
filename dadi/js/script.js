766fd4d8f9fe8fc08b199b9d528c880ffb8837b2
Francesco <francescofogliajj@gmail.com>
2020-11-21 17:07:17 +0100

    Commit #4

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,4 +1,4 @@
-// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 var giocatore = Math.floor(Math.random() * 6) + 1;
 console.log(giocatore + " - Numero del giocatore");
 
@@ -8,8 +8,11 @@ console.log(computer + " - Numero del computer");
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 if (giocatore > computer) {
   console.log(giocatore + " è maggiore di " + computer + " - Hai vinto!");
+  alert("Hai vinto!");
 } else if (giocatore < computer) {
   console.log(giocatore + " è minore di " + computer + " - Hai perso!");
+  alert("Hai perso!");
 } else if (giocatore = computer) {
   console.log(giocatore + " è uguale a " + computer + " - Pareggio!");
+  alert("Pareggio!");
 }
